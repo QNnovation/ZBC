8dcedb987c2526fb22fc562052bd0f7d918a6fb2
Anton Melnyk <an.melnik@gmail.com>
2015-11-22 00:05:20 +0200

    In progress: try to use fusion style

--- zbc_mainwindow.cpp
+++ zbc_mainwindow.cpp
@@ -1,9 +1,14 @@
+#include <QDebug>
+
 #include "zbc_mainwindow.h"
 #include "zbc_centralwidget.h"
 
+#include <QApplication>
 #include <QMenuBar>
+#include <QPalette>
 #include <QToolBar>
 #include <QSettings>
+#include <QStyleFactory>
 
 
 //C-tor
@@ -38,6 +43,26 @@ ZBC_MainWindow::ZBC_MainWindow(QWidget* pwgt) : QMainWindow(pwgt)
         this->restoreGeometry(Settings.value("/Geometry").toByteArray());
     Settings.endGroup();
     Settings.endGroup();
+
+//Debug BEGIN
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+    QPalette darkPalette;
+        darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+        darkPalette.setColor(QPalette::WindowText, Qt::white);
+        darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+        darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+        darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+        darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+        darkPalette.setColor(QPalette::Text, Qt::white);
+        darkPalette.setColor(QPalette::Button, QColor(0,102,0));
+        darkPalette.setColor(QPalette::ButtonText, Qt::white);
+        darkPalette.setColor(QPalette::BrightText, Qt::red);
+        darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    QApplication::setPalette(darkPalette);
+    qDebug() << QStyleFactory::keys();
+//Debug END
 }
 
 
