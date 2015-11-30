e0b2d770c3a45b3f7fc413fc08c4566802c3b8d3
Lysytskyi Oleksii <folax@ukr.net>
2015-11-30 15:36:19 +0200

    memory leaks fixed

--- wgtfilessearch.cpp
+++ wgtfilessearch.cpp
@@ -23,26 +23,26 @@ wgtFilesSearch::wgtFilesSearch(const QString &path, QWidget *parent) : QDialog(p
     else
         dirPath = path;
 
-    m_searchFileLbl = new QLabel(tr("Files:"));
-    m_pathToFileLbl = new QLabel(tr("Path:"));
-    m_searchFileEdit = new QLineEdit();
-    m_pathToFileEdit = new QLineEdit();
+    m_searchFileLbl = new QLabel(tr("Files:"), this);
+    m_pathToFileLbl = new QLabel(tr("Path:"), this);
+    m_searchFileEdit = new QLineEdit(this);
+    m_pathToFileEdit = new QLineEdit(this);
     m_pathToFileEdit->setText(dirPath);
 
-    m_withTextLbl = new QLabel(tr("With text:"));
-    m_withTextEdit = new QLineEdit();
+    m_withTextLbl = new QLabel(tr("With text:"), this);
+    m_withTextEdit = new QLineEdit(this);
 
-    m_withTextOption = new QCheckBox();
-    m_caseSensOption = new QCheckBox(tr("Case sensitive"));
-    m_wholeWordsOption = new QCheckBox(tr("Only whole words"));
+    m_withTextOption = new QCheckBox(this);
+    m_caseSensOption = new QCheckBox(tr("Case sensitive"), this);
+    m_wholeWordsOption = new QCheckBox(tr("Only whole words"), this);
 
-    m_upGridLayout = new QGridLayout();
+    m_upGridLayout = new QGridLayout(this);
     m_upGridLayout->addWidget(m_searchFileLbl, 0, 0);
     m_upGridLayout->addWidget(m_searchFileEdit, 0, 1);
     m_upGridLayout->addWidget(m_pathToFileLbl, 1, 0);
     m_upGridLayout->addWidget(m_pathToFileEdit, 1, 1);
 
-    QFrame *lineOnWgt = new QFrame();
+    QFrame *lineOnWgt = new QFrame(this);
     lineOnWgt->setFrameStyle(QFrame::HLine | QFrame::Raised);
     m_upGridLayout->addWidget(lineOnWgt, 2, 0, 1, 4);
 
@@ -53,40 +53,40 @@ wgtFilesSearch::wgtFilesSearch(const QString &path, QWidget *parent) : QDialog(p
     m_upGridLayout->addWidget(m_wholeWordsOption, 5, 0);
 
     //tab pages
-    m_generalTab = new QWidget();
+    m_generalTab = new QWidget(this);
     m_generalTab->setLayout(m_upGridLayout);
 
     //tab
-    m_tabWgt = new QTabWidget();
+    m_tabWgt = new QTabWidget(this);
     m_tabWgt->addTab(m_generalTab, QString(tr("General")));
     m_tabWgt->addTab(new QLabel(tr("2")), QString(tr("Options")));
 
-    m_upTopLayout = new QHBoxLayout;
+    m_upTopLayout = new QHBoxLayout();
     m_upTopLayout->addWidget(m_tabWgt);
 
-    m_rightBtnLayout = new QVBoxLayout();
-    m_btnSearch = new QPushButton(tr("Search"));
-    m_btnCancel = new QPushButton(tr("Cancel"));
+    m_rightBtnLayout = new QVBoxLayout(this);
+    m_btnSearch = new QPushButton(tr("Search"), this);
+    m_btnCancel = new QPushButton(tr("Cancel"), this);
     m_rightBtnLayout->addSpacing(20);
     m_rightBtnLayout->addWidget(m_btnSearch);
     m_rightBtnLayout->addWidget(m_btnCancel);
     m_rightBtnLayout->addStretch();
     m_upTopLayout->addLayout(m_rightBtnLayout);
 
-    m_mainBoxLayout = new QVBoxLayout();
-    m_foundFileList = new QListWidget();
+    m_mainBoxLayout = new QVBoxLayout(this);
+    m_foundFileList = new QListWidget(this);
     m_foundFileList->addItem("d:/");
     //listWgt->setVisible(false);
     m_mainBoxLayout->addLayout(m_upTopLayout);
-    m_searchPathLbl = new QLabel(tr(":"));
+    m_searchPathLbl = new QLabel(tr(":"), this);
 
     m_mainBoxLayout->addWidget(m_searchPathLbl);
     m_mainBoxLayout->addWidget(m_foundFileList);
 
     m_bottomBtnLayout = new QHBoxLayout();
-    m_btnViewFile = new QPushButton(tr("View"));
-    m_btnGotoFile = new QPushButton(tr("Go to file"));
-    m_btnNewSearch = new QPushButton(tr("New Search"));
+    m_btnViewFile = new QPushButton(tr("View"), this);
+    m_btnGotoFile = new QPushButton(tr("Go to file"), this);
+    m_btnNewSearch = new QPushButton(tr("New Search"), this);
     m_bottomBtnLayout->addWidget(m_btnViewFile);
     m_bottomBtnLayout->addWidget(m_btnGotoFile);
     m_bottomBtnLayout->addWidget(m_btnNewSearch);
